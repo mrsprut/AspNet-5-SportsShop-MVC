dc479044d2a7f7a74e1b5e09fbac9b86fc6977cd
YuriiTrofimenko <tyaa@ukr.net>
2021-07-05 07:40:52 +0300

    Setup MVC

---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
