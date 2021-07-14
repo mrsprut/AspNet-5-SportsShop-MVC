344bdd3453bea089822f42ec969b25074edba26b
YuriiTrofimenko <tyaa@ukr.net>
2021-07-14 12:05:51 +0300

    product list categories nav panel using component - step 1

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.Components {
+    public class NavigationMenuViewComponent : ViewComponent {
+        public string Invoke() {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
