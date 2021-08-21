fee794ef702d9f0e2012ab3c86a2ad26ed83ed00
YuriiTrofimenko <tyaa@ukr.net>
2021-08-21 17:31:19 +0300

    Checkout

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -29,6 +29,7 @@ namespace SportsStore
                     Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddScoped<IOrderRepository, EFOrderRepository>();
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
