c4bed16717fe891c0195e5d1299d7618162a6231
YuriiTrofimenko <tyaa@ukr.net>
2021-08-26 17:02:11 +0300

    admin sections using Blazor Server - Setup

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -35,6 +35,7 @@ namespace SportsStore
             services.AddSession();
             services.AddScoped(SessionCart.GetCart);
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +65,8 @@ namespace SportsStore
                     new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
             
             SeedData.EnsurePopulated(app);
