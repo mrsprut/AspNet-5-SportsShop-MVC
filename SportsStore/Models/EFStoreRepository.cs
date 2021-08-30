a255e2d4a60b92fcec1b2a4ba472461ee7ad60be
YuriiTrofimenko <tyaa@ukr.net>
2021-08-30 17:48:03 +0300

    admin components: products

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -7,6 +7,18 @@ namespace SportsStore.Models
         private StoreDbContext context;
         public EFStoreRepository(StoreDbContext ctx) {
             context = ctx;
-        }public IQueryable<Product> Products => context.Products;
+        }
+        public IQueryable<Product> Products => context.Products;
+        public void CreateProduct(Product p) {
+            context.Add(p);
+            context.SaveChanges();
+        }
+        public void DeleteProduct(Product p) {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+        public void SaveProduct(Product p) {
+            context.SaveChanges();
+        }
     }
 }
